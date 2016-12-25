fd650dedd1375f77b97d06d108bae2c905e1660d
Karnkuret <ricardo.avila.carrillo@gmail.com>
2016-12-25 15:55:53 -0800

    Added soundpool and soundpool constructor

--- app/src/main/java/com/example/ricardo/beatbox/BeatBox.java
+++ app/src/main/java/com/example/ricardo/beatbox/BeatBox.java
@@ -2,6 +2,8 @@ package com.example.ricardo.beatbox;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -17,12 +19,15 @@ public class BeatBox {
     private static final String TAG = "BeatBox";
 
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
     }
 
@@ -43,7 +48,7 @@ public class BeatBox {
         }
     }
 
-    public List<Sound> getSounds(){
+    public List<Sound> getSounds() {
         return mSounds;
     }
 }
