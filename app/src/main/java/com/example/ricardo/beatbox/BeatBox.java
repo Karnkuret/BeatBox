74959fe880f8f5e0717d75088f41f751f43c7ecb
Karnkuret <ricardo.avila.carrillo@gmail.com>
2016-12-25 16:18:08 -0800

    *Added play() & load() to BeatBox.java in order to load and play sounds. *Added unloading sounds methods in order to be a good and clean citizen *Implemented onClickListener on SoundHolder class inside BeatBoxFragment.java *Added a setRetainInstance to retain the fragment *Added getter and setter for mSoundId *Ended chapter 19 of the book

--- app/src/main/java/com/example/ricardo/beatbox/BeatBox.java
+++ app/src/main/java/com/example/ricardo/beatbox/BeatBox.java
@@ -1,6 +1,7 @@
 package com.example.ricardo.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -31,6 +32,18 @@ public class BeatBox {
         loadSounds();
     }
 
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void release() {
+        mSoundPool.release();
+    }
+
     private void loadSounds() {
         String[] soundNames;
         try {
@@ -42,10 +55,21 @@ public class BeatBox {
         }
 
         for (String filename : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Could not load sound " + filename, ioe);
+            }
         }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
